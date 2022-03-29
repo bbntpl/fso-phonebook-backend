3e82f7cb3cc24aa54ddd387e0fb6a3e5f6c69e7c
bvrbryn445 <law911012eqw@gmail.com>
2022-03-29 19:00:11 -0400

    add exercise 3.3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
@@ -33,7 +34,17 @@ app.get('/info', (req, res) => {
 	const str = `<p>Phonebook has info for ${persons.length} people<p> 
 	<p>${new Date()}</p>`;
 	res.send(str);
-})
+});
+
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id);
+	const person = persons.find(person => person.id === id);
+	if(person) {
+		res.json(person);
+	} else {
+		res.status(404).end();
+	}
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
